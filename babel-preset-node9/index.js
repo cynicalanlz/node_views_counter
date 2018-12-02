2679f7c1a84e6f4b786b8976694774942151ca13
cynicalanlz <adsnsrg@gmail.com>
2018-12-02 19:44:20 +0300

    init

---
+++ babel-preset-node9/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    require("babel-preset-es2015"),
+  ],
+  plugins: [
+    require("babel-preset-es2015")
+    require("babel-plugin-literals")
+    require("babel-plugin-spread")
+    require("babel-plugin-sticky-regex")
+    require("babel-plugintemplate-literals")
+  ]
+};
