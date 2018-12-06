bb9b069c8b179118215c108bc991928961026918
cynicalanlz <adsnsrg@gmail.com>
2018-12-06 19:04:41 +0300

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+#Last minute pageviews counter
+
+# How to deploy to production
+```
+git clone  https://github.com/cynicalanlz/node_views_counter
+git commit ...
+git push origin master
+```
+
+#
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
-// const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
